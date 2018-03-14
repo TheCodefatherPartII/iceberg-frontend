745f762ff628e83f33d28c82512745c08510f419
Esteban Salazar <esteban.salazar@terem.com.au>
2018-03-14 18:13:19 +1100

    Add styles file

---
+++ src/config/mapStyles.js
@@ -0,0 +1,25 @@
+const styles = [
+  {
+    "featureType": "all",
+    "elementType": "all",
+    "stylers": [
+      {
+        "invert_lightness": true
+      },
+      {
+        "saturation": 10
+      },
+      {
+        "lightness": 30
+      },
+      {
+        "gamma": 0.5
+      },
+      {
+        "hue": 3.14
+      }
+    ]
+  }
+];
+
+export default styles;
