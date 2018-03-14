7a199ccbf90590c1c1bec9a9198059af199e8133
Esteban Salazar <esteban.salazar@terem.com.au>
2018-03-14 17:39:51 +1100

    Add Tom's map styles

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -3,6 +3,7 @@
     :center="center"
     :zoom="7"
     style="width: 1024px; height: 640px"
+    :options="{styles: mapStyles}"
     ref="map"
   >
     <gmap-marker
@@ -17,43 +18,474 @@
 </template>
 
 <script>
-import * as VueGoogleMaps from 'vue2-google-maps';
-import {loaded} from 'vue2-google-maps'
-import Vue from 'vue';
-import providers from '../providers';
+import * as VueGoogleMaps from "vue2-google-maps";
+import { loaded } from "vue2-google-maps";
+import Vue from "vue";
+import providers from "../providers";
 
 Vue.use(VueGoogleMaps, {
   load: {
-    key: 'AIzaSyBRFyLekDBT0Zh0fF0I2zpcto38orRJ5OA',
+    key: "AIzaSyBRFyLekDBT0Zh0fF0I2zpcto38orRJ5OA"
     // libraries: 'places', //// If you need to use place input
   }
 });
 
+const styles = [
+  {
+    featureType: "all",
+    elementType: "all",
+    stylers: [
+      {
+        visibility: "on"
+      }
+    ]
+  },
+  {
+    featureType: "all",
+    elementType: "labels",
+    stylers: [
+      {
+        visibility: "off"
+      },
+      {
+        saturation: "-100"
+      }
+    ]
+  },
+  {
+    featureType: "all",
+    elementType: "labels.text.fill",
+    stylers: [
+      {
+        saturation: 36
+      },
+      {
+        color: "#000000"
+      },
+      {
+        lightness: 40
+      },
+      {
+        visibility: "off"
+      }
+    ]
+  },
+  {
+    featureType: "all",
+    elementType: "labels.text.stroke",
+    stylers: [
+      {
+        visibility: "off"
+      },
+      {
+        color: "#000000"
+      },
+      {
+        lightness: 16
+      }
+    ]
+  },
+  {
+    featureType: "all",
+    elementType: "labels.icon",
+    stylers: [
+      {
+        visibility: "off"
+      }
+    ]
+  },
+  {
+    featureType: "administrative",
+    elementType: "geometry.fill",
+    stylers: [
+      {
+        color: "#000000"
+      },
+      {
+        lightness: 20
+      }
+    ]
+  },
+  {
+    featureType: "administrative",
+    elementType: "geometry.stroke",
+    stylers: [
+      {
+        color: "#000000"
+      },
+      {
+        lightness: 17
+      },
+      {
+        weight: 1.2
+      }
+    ]
+  },
+  {
+    featureType: "landscape",
+    elementType: "geometry",
+    stylers: [
+      {
+        color: "#000000"
+      },
+      {
+        lightness: 20
+      }
+    ]
+  },
+  {
+    featureType: "landscape",
+    elementType: "geometry.fill",
+    stylers: [
+      {
+        color: "#4d6059"
+      }
+    ]
+  },
+  {
+    featureType: "landscape",
+    elementType: "geometry.stroke",
+    stylers: [
+      {
+        color: "#4d6059"
+      }
+    ]
+  },
+  {
+    featureType: "landscape.natural",
+    elementType: "geometry.fill",
+    stylers: [
+      {
+        color: "#4d6059"
+      }
+    ]
+  },
+  {
+    featureType: "poi",
+    elementType: "geometry",
+    stylers: [
+      {
+        lightness: 21
+      }
+    ]
+  },
+  {
+    featureType: "poi",
+    elementType: "geometry.fill",
+    stylers: [
+      {
+        color: "#4d6059"
+      }
+    ]
+  },
+  {
+    featureType: "poi",
+    elementType: "geometry.stroke",
+    stylers: [
+      {
+        color: "#4d6059"
+      }
+    ]
+  },
+  {
+    featureType: "road",
+    elementType: "geometry",
+    stylers: [
+      {
+        visibility: "on"
+      },
+      {
+        color: "#7f8d89"
+      }
+    ]
+  },
+  {
+    featureType: "road",
+    elementType: "geometry.fill",
+    stylers: [
+      {
+        color: "#7f8d89"
+      }
+    ]
+  },
+  {
+    featureType: "road.highway",
+    elementType: "geometry.fill",
+    stylers: [
+      {
+        color: "#7f8d89"
+      },
+      {
+        lightness: 17
+      }
+    ]
+  },
+  {
+    featureType: "road.highway",
+    elementType: "geometry.stroke",
+    stylers: [
+      {
+        color: "#7f8d89"
+      },
+      {
+        lightness: 29
+      },
+      {
+        weight: 0.2
+      }
+    ]
+  },
+  {
+    featureType: "road.arterial",
+    elementType: "geometry",
+    stylers: [
+      {
+        color: "#000000"
+      },
+      {
+        lightness: 18
+      }
+    ]
+  },
+  {
+    featureType: "road.arterial",
+    elementType: "geometry.fill",
+    stylers: [
+      {
+        color: "#7f8d89"
+      }
+    ]
+  },
+  {
+    featureType: "road.arterial",
+    elementType: "geometry.stroke",
+    stylers: [
+      {
+        color: "#7f8d89"
+      }
+    ]
+  },
+  {
+    featureType: "road.local",
+    elementType: "geometry",
+    stylers: [
+      {
+        color: "#000000"
+      },
+      {
+        lightness: 16
+      }
+    ]
+  },
+  {
+    featureType: "road.local",
+    elementType: "geometry.fill",
+    stylers: [
+      {
+        color: "#7f8d89"
+      }
+    ]
+  },
+  {
+    featureType: "road.local",
+    elementType: "geometry.stroke",
+    stylers: [
+      {
+        color: "#7f8d89"
+      }
+    ]
+  },
+  {
+    featureType: "transit",
+    elementType: "geometry",
+    stylers: [
+      {
+        color: "#000000"
+      },
+      {
+        lightness: 19
+      }
+    ]
+  },
+  {
+    featureType: "water",
+    elementType: "all",
+    stylers: [
+      {
+        color: "#2b3638"
+      },
+      {
+        visibility: "on"
+      }
+    ]
+  },
+  {
+    featureType: "water",
+    elementType: "geometry",
+    stylers: [
+      {
+        color: "#2b3638"
+      },
+      {
+        lightness: 17
+      }
+    ]
+  },
+  {
+    featureType: "water",
+    elementType: "geometry.fill",
+    stylers: [
+      {
+        color: "#24282b"
+      }
+    ]
+  },
+  {
+    featureType: "water",
+    elementType: "geometry.stroke",
+    stylers: [
+      {
+        color: "#24282b"
+      }
+    ]
+  },
+  {
+    featureType: "water",
+    elementType: "labels",
+    stylers: [
+      {
+        visibility: "off"
+      }
+    ]
+  },
+  {
+    featureType: "water",
+    elementType: "labels.text",
+    stylers: [
+      {
+        visibility: "off"
+      }
+    ]
+  },
+  {
+    featureType: "water",
+    elementType: "labels.text.fill",
+    stylers: [
+      {
+        visibility: "off"
+      }
+    ]
+  },
+  {
+    featureType: "water",
+    elementType: "labels.text.stroke",
+    stylers: [
+      {
+        visibility: "off"
+      }
+    ]
+  },
+  {
+    featureType: "water",
+    elementType: "labels.icon",
+    stylers: [
+      {
+        visibility: "off"
+      }
+    ]
+  }
+];
+
 export default {
-  data () {
+  data() {
     const mapConfig = providers.reduce((currentConfig, provider) => {
       const providerConfig = provider(currentConfig);
-      return ({
+      return {
         ...currentConfig,
         ...providerConfig,
         markers: [
-          ...(currentConfig.markers || []), ...(providerConfig.markers || [])
+          ...(currentConfig.markers || []),
+          ...(providerConfig.markers || [])
         ]
-      })
+      };
     }, {});
     return mapConfig;
   },
   mounted() {
     const map = this.$refs.map;
     loaded.then(() => {
-      let trafficLayer = new google.maps.TrafficLayer;
+      let trafficLayer = new google.maps.TrafficLayer();
       map.$mapCreated.then(theMap => {
         trafficLayer.setMap(theMap);
       });
     });
-  }
-}
+  },
+  computed: {
+    mapStyles() {
+      return styles;
+      /*
+      return [
+        {
+          stylers: [
+            {
+              hue: "#899999"
+            },
+            {
+              visibility: "on"
+            },
+            {
+              gamma: 0.5
+            },
+            {
+              weight: 0.5
+            }
+          ]
+        },
+        {
+          featureType: "road",
+          stylers: [
+            {
+              visibility: "off"
+            }
+          ]
+        },
+        {
+          featureType: "transit.line",
+          stylers: [
+            {
+              color: "#FF0000"
+            }
+          ]
+        },
+        {
+          featureType: "poi",
+          elementType: "labels.icon",
+          stylers: [
+            {
+              visibility: "on"
+            },
+            {
+              weight: 10
+            }
+          ]
+        },
+        {
+          featureType: "water",
+          stylers: [
+            {
+              color: "#8900FF"
+            },
+            {
+              weight: 9999900000
+            }
+          ]
+        }
+      ];
 
+      */
+    }
+  }
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
